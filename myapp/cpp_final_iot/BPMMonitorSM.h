5312f4af92af8f46de535a7c55f0a99282abf31d
Amir Asdaghpour <57164807+amirmasp@users.noreply.github.com>
2023-01-03 22:08:21 +0000

    deleted:    myapp/Particle_Code/BPMMonitorSM.cpp 	deleted:    myapp/Particle_Code/BPMMonitorSM.h 	deleted:    myapp/Particle_Code/ece413513-demo.ino 	deleted:    myapp/Particle_Code/project.properties 	modified:   myapp/cpp_final_iot/BPMMonitorSM.h

--- myapp/cpp_final_iot/BPMMonitorSM.h
+++ myapp/cpp_final_iot/BPMMonitorSM.h
@@ -1,25 +1,25 @@
-//-------------------------------------------------------------------
-
 #ifndef BPMMonitorSM_H
 #define BPMMonitorSM_H
-
-//-------------------------------------------------------------------
 
 #include <vector>
 #include <Wire.h>
 #include <time.h>
 #include "MAX30105.h"
 
-//-------------------------------------------------------------------
-
 using namespace std;
 
-//-------------------------------------------------------------------
+class BPMMonitorSM { 
+   public:
+   BPMMonitorSM(MAX30105& mySensor);
+   int currentFrequency;
+   int startTime;
+   int endTime;
+   int updatedFreqCheck;
+   void execute();
+   float getBPM();
 
-class BPMMonitorSM {
+   private:
    enum State { S_Init, S_ReadSensor, S_Report};
-
-private:
    State state;
    long lastBeat;
    int tick;
@@ -29,34 +29,15 @@ private:
    vector<float> spo2History;
    bool Readyforgdata;
    int readingTimer;
-
-   //new
    int noWiFiTick;
    int fiveMinutesTick;
    int frequencyTick;//in minutes
-
-
-
-    uint32_t irBuffer[100]; //infrared LED sensor data
-    uint32_t redBuffer[100];  //red LED sensor data
-
-    int32_t bufferLength; //data length
-    int32_t spo2; //SPO2 value
-    int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
-    int32_t heartRate; //heart rate value
-    int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
-
-
-public:
-   BPMMonitorSM(MAX30105& mySensor);
-   int currentFrequency;
-   int startTime;
-   int endTime;
-   int updatedFreqCheck;
-   void execute();
-   float getBPM();
+   uint32_t irBuffer[100]; //infrared LED sensor data
+   uint32_t redBuffer[100];  //red LED sensor data
+   int32_t bufferLength; //data length
+   int32_t spo2; //SPO2 value
+   int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+   int32_t heartRate; //heart rate value
+   int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
 };
-
-//-------------------------------------------------------------------
-
 #endif
